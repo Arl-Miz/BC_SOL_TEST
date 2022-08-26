27a8e7ef02b357f2781e8f07be1ca1ea5276aaac
Remix Workspace <>
2022-08-27 00:15:38 +0430

    added ERC20Tkn

---
+++ Shop9fy_flat.sol
@@ -0,0 +1,60 @@
+//SPDX-License-Identifier: UNLICENSED
+// File: contracts/Shop9fy.sol
+
+
+pragma solidity ^0.8.8;
+
+
+contract SeulAot{
+
+// memeory/mapping/enums/...
+// enums set limits
+
+
+    uint256 monNumero ;
+
+    mapping(address => uint256) public addressToNum;
+    // mapping(address => string) public addressToName;
+
+    // mapping(uint256 => address) public numberToAddress;
+    // mapping(string => address) public nameToAddress;
+
+
+    enum bNCurse {bn1,bn2,bn3}
+    bNCurse currentChoice;
+    bNCurse constant defualtChoice = bNCurse.bn3;
+    
+    function set1() public {
+        currentChoice = bNCurse.bn2;
+    }
+    function getCurse() public view returns(bNCurse){
+        return currentChoice;
+        
+    }
+
+    // store number
+    // fn get user number by address
+    // fn get user name by address
+    // fn export user number by address
+    // fn export user name by address
+
+    function getUNBA(uint256 _num) public  returns(uint){
+         addressToNum[msg.sender] = _num;
+        //  addressToName[msg.sender] = _name;
+        return(addressToNum[msg.sender]);
+        // addressToName[msg.sender] = _name;
+    
+    }
+
+    // function exUNBA(uint256 _num,string memory _name) public view returns(uint256 add_) {
+
+    //     // add_ = addressToName;
+
+    //     return(
+    //     // addressToName[_num] = msg.sender 
+
+    //     );
+    // }
+
+
+}
---
+++ contracts/TokensTry/ERC20Token.sol
@@ -0,0 +1,117 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.6;
+
+abstract contract ERC20Token{
+    function name() virtual public view returns(string memory);
+    function symbol() virtual public view returns(string memory);
+    function decimals() virtual public view returns(uint8);
+    function totalSupply() virtual public view returns(uint256);
+    function balanceOf(address _owner) virtual public view returns (uint256 balance);
+    function transfer(address _to, uint256 _value) virtual public returns(bool success);
+    function transferFrom(address _sender, address _to, uint256 _amount) virtual public returns (bool success);
+    function approve(address _spender, uint256 _amount) virtual public returns (bool success);
+    function allowance(address _owner, address _spender)virtual public view returns (uint256 remaining);
+
+
+    event Transfer(address indexed _from, address indexed _to, uint256 value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 value);
+}
+
+contract owned{
+
+    address public owner;
+    address public newOwner;
+
+    event ownershipTransfer(address indexed _from, address indexed _to);
+
+    constructor() {
+        owner = msg.sender;
+    }   
+
+    function transferOwnership(address _to) public{
+        require(msg.sender == owner);
+        newOwner = _to;
+    }
+
+    function acceptOwnership() public{
+        require(msg.sender == newOwner);
+        emit ownershipTransfer(owner, newOwner);
+        owner = newOwner;
+        newOwner = address(0);
+    }  
+}
+
+contract Token is ERC20Token, owned{
+    string public _symbol;
+    string public _name;
+    uint8 public _decimals;
+    uint256 public _totalSupply;
+    address public _minter;
+
+    mapping(address => uint256) public balances;
+
+    constructor (){
+        _symbol="TTK";
+        _name = "Test Token";
+        _decimals = 0;
+        _totalSupply = 100;
+        _minter = 0xe2f06255e2215199fCCC0A7B30376b56f0630A13;
+    }
+    function name() public override  view returns(string memory){
+        return  _name;
+    }
+    function symbol() public override view returns(string memory){
+        return _symbol;
+    }
+    function decimals() public override  view returns(uint8){
+        return _decimals;
+    }
+    function totalSupply() public override view returns(uint256){
+        return _totalSupply;
+    }
+    function balanceOf(address _owner) public override view returns (uint256 balance){
+        return balances[_owner];
+    }
+    
+    function transferFrom(address _from, address _to, uint256 _value) public override returns(bool success){
+        require(balances[_from] >=  _value);
+        balances[_from] -= _value;
+        balances[_to] += _value;
+        emit Transfer(_from,_to,_value);
+
+        return true;
+    }
+
+    function transfer(address _to, uint256 _value) public override returns(bool success){
+        return transferFrom(msg.sender, _to, _value);
+    }
+
+    function approve(address _spender, uint256 _value) public override returns(bool success){
+        return true;
+    }
+    function allowance(address _owner, address _spender) public override view returns(uint256 remaining){
+        return 0;
+    }
+
+    function mint(uint amount) public returns(bool){
+        require(msg.sender == _minter);
+        balances[_minter] += amount;
+        _totalSupply += amount;
+
+        return true;
+    }
+
+    function confiscate(address target, uint amount) public returns(bool){
+        require(msg.sender == _minter);
+
+        if(balances[target] >= amount ){
+            balances[target] -= amount;
+            _totalSupply -= amount;
+        }else{
+            _totalSupply = balances[target];
+            balances[target] =0;
+        }
+    }
+
+}
